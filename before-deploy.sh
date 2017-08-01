435a6e55812647be7efa34e3e3d305614b7fe039
Tim Etchells <etim@ca.ibm.com>
2017-08-01 10:38:36 -0400

    upload to bintray for real

---
+++ before-deploy.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Find and replace placeholders in bintray.json with real values
+
+bintray_file="./bintray.json"
+echo "before-deploy.sh - editing $bintray_file" 
+
+date=$(date +"%F")
+echo "Date is $date"
+sed -i -e "s/__released__/$date/g" $bintray_file
+
+build_tag=$TRAVIS_TAG
+if [ ! -z "$TRAVIS_TAG" ]; then
+    echo "Travis tag is $TRAVIS_TAG"
+else
+    echo "No Travis tag"
+    if [ ! -z "$TRAVIS_COMMIT" ]; then
+        short_commit=${TRAVIS_COMMIT:0:8}
+        echo "Using commit $short_commit" 
+        build_tag="$short_commit"
+    else
+        echo "ERROR: TRAVIS_COMMIT is empty"
+    fi
+fi
+
+sed -i -e "s/__vcs_tag__/$build_tag/g" $bintray_file
+upload_dir=""
+if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
+    upload_dir="\/push"
+elif [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+    upload_dir="\/release"
+else
+    upload_dir="\/personal"
+fi
+
+sed -i -e "s/__upload_dir__/$upload_dir/g" $bintray_file
+
+echo "Finished updating $bintray_file"
+echo "Contents:"
+cat "$bintray_file"
