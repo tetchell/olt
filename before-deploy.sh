0137cb4774eb08f2363ca98b26bc710cfdec7d3a
Tim Etchells <etim@ca.ibm.com>
2017-08-02 17:47:17 -0400

    Version builds

--- before-deploy.sh
+++ before-deploy.sh
@@ -14,10 +14,9 @@ find_replace_in_file() {
 bintray_file="./bintray.json"
 echo "before-deploy.sh - editing $bintray_file" 
 
-date=$(date +"%F")
+date=$(date +%F)
 #echo "Date is $date"
 find_replace_in_file $bintray_file __released__ $date
-
 
 build_tag=$TRAVIS_TAG
 if [ ! -z "$TRAVIS_TAG" ]; then
@@ -29,23 +28,26 @@ else
 #        echo "Using commit $short_commit as tag" 
         build_tag="$short_commit"
     else
+        # This will never happen in a real build
         echo "ERROR: TRAVIS_COMMIT is empty"
+        build_tag=$(date +%s)
     fi
 fi
 
-find_replace_in_file $bintray_file __vcs_tag__ $build_tag
+#find_replace_in_file $bintray_file __vcs_tag__ $build_tag
 
 package=""
+version_tag=""
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
     package="olt-nightly"
+    version_tag="nightly_"$(date +%Y%m%d)
 else
     package="olt-ci"
+    version_tag="ci_$build_tag"
 fi
 
-sed -i -e "s/__package__/$package/g" $bintray_file
-
 find_replace_in_file $bintray_file __package__ $package
-find_replace_in_file $bintray_file __version_name__ $package
+find_replace_in_file $bintray_file __version__ $version_tag
 
 echo "Finished updating $bintray_file"
 echo "Contents:"
