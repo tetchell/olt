ac74d06eec68046a8c86f823bb9d8f3dd98dee78
Tim Etchells <etim@ca.ibm.com>
2017-08-22 14:35:20 -0400

    Upload test_results.html and info.json

--- before-deploy.sh
+++ before-deploy.sh
@@ -33,6 +33,33 @@ fi
 find_replace_in_file $bintray_file __package__ $package
 find_replace_in_file $bintray_file __version__ $version_tag
 
+path_to_artifacts="build/libs/"     # TODO replace this with dev/ant_build/artifacts
+
 echo "Finished updating $bintray_file"
 echo "Contents:"
 cat "$bintray_file"
+
+# Now update info.json - This will actually be done by Couplet.
+
+info_json="./info.json"
+# Determine the name of the updatesite (NOT test updatesite) 
+# - this must be specified in info.json
+#artifact_base_name="open-wdt-update-site*"
+artifact_base_name="olt*"
+# Find matching file, then just get the file name (basename) 
+# since everything will be in one dir once deployed on bintray
+artifact_matching="$(ls $path_to_artifacts$artifact_base_name | xargs -n 1 basename)"
+
+echo "Updatesite zip is $artifact_matching"
+if [ -z "$artifact_matching" ]; then
+    echo "ERROR: No file matching $path_to_artifacts$artifact_base_name was found. The updatesite is missing!"
+    exit 1
+fi
+
+
+
+find_replace_in_file $info_json __driver_location__ $artifact_matching
+
+echo "Finished updating $info_json"
+echo "Contents:"
+cat $info_json
